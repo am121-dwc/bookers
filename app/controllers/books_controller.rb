590f0edc63949204d7f9330e06913351cb159116
am121-dwc <aoimorisugi121@gmail.com>
2023-12-13 09:09:52 +0000

    home almost finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,32 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  def create
+    book = Book.new(list_params)
+    book.save
+    redirect_to '/'
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
