df51cc9bc8f7c8a06d3e92d8f70de5efc1d2339b
am121-dwc <aoimorisugi121@gmail.com>
2023-12-15 10:55:57 +0000

    flash message almost finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,25 +18,43 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      # 　　　　　　　　　↓　修正する可能性あり
+       redirect_to book_path(book.id)  
+    else
+      flash.now[:notice] = "Book was not created."
+      render :index
+    end
   end
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path
+    if @book.update(book_params)
+       flash[:notice] = "Book was successfully updated."
+       redirect_to book_path
+    else
+       flash.now[:notice] = "Book was not updated."
+        # 　　　　　　↓　修正する可能性あり
+       redirect_to book_path
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to books_path
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    else
+      flash.now[:notice] = "book was not destroyed."
+       # 　　　　　　　↓　修正する可能性あり
+      redirect_to books_path
+    end
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 end
